0ef357c9adef4ab7624551e3d17c8265ac3b669c
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-11-20 13:09:30 -0800

    grab assets.did

---
+++ external/update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+# This will pull the latest asset.did file from the Rust Asset Canister
+# https://github.com/dfinity/sdk/tree/master/src/canisters/frontend/ic-certified-assets
+curl -O https://raw.githubusercontent.com/dfinity/sdk/master/src/canisters/frontend/ic-certified-assets/assets.did
+
+
